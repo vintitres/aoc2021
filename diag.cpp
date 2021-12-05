37f4fdf44a05b7010adbd6326c4576e44d87b280
Jakub Woyke <jaqb@sekta.pl>
2021-12-05 04:30:55 +0000

    ugh

--- diag.cpp
+++ diag.cpp
@@ -1,31 +1,66 @@
 #include<iostream>
+#include<set>
 #include<vector>
 using namespace std;
 
-void f(const string &s, vector<int> &v) {
-  for (int i =0; i <= s.size(); ++i){
-    if(s[i]=='0')++v[i];
+
+
+void f(const string &s, vector<int> &v, set<int>&ss) {
+  int vv=0,i=0;
+  for (auto it=s.begin(); it != s.end(); ++it){
+    vv=vv<<1;
+    if (*it=='1'){++vv;++v[i];}
     else --v[i];
+    ++i;
+  }
+  ss.insert(vv);
+}
+
+void ff(set<int> &vals, vector<int> &bit_sums, int p, bool b) {
+
+  cout << (1<<p) << "p " << b << "b ";
+  for (auto it =vals.begin();it!=vals.end();) {
+    cout << *it;
+    if(((bool)((*it)&(1<<p))) == b) {
+      cout << "k";
+      ++it;
+    }
+    else {
+      int v =*it;
+      for (auto itt = bit_sums.rbegin(); itt != bit_sums.rend(); ++itt){
+        if (v %2) --*itt;
+        else ++*itt;
+        v/=2;
+      }
+      it =vals.erase(it); 
+    }
+    cout << ' ';
   }
 }
 
-  int main(){
-    string s;
-    cin>>s;
-    vector<int> x(s.size());
-    f(s,x);
-      while (cin >> s) {
-        f(s, x);
-      }
-    int d = 1, v=0;
-    for (auto it=x.begin(); it!=x.end(); ++it){
-      cout <<*it <<endl;
-      if (*it>0) ++v;
-      v=v<<1;
-    }
-    v=v>>1;
-     int vv =  ((1<<(x.size()))-1)^v;
-        cout << v << endl<< vv << endl <<
-      v * vv<< endl;
+int fff(set<int> vals, vector<int> bit_sums, bool a, bool b) {
+  int i = bit_sums.size() -1;
+  for (auto it=bit_sums.begin(); it!=bit_sums.end(); ++it){
+    cout << *it << endl;
+    ff(vals, bit_sums, i,*it>=0?a:b);
+    if(vals.size()==1)return *(vals.begin());
+    cout << endl;
+    --i;
   }
+
+}
+int main(){
+  string s;
+  cin>>s;
+  vector<int> x(s.size());
+  set<int>ss;
+  f(s,x,ss);
+  while (cin >> s) {
+    f(s, x,ss);
+  }
+  int ssv = fff(ss, x, 1, 0);
+  int sssv = fff(ss, x, 0, 1);
+    cout << ssv << " " <<sssv<< endl;
+  cout << ssv*sssv<< endl;
+}
 
