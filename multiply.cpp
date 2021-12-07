0fdff65269d521ec72258a2c3269ec4cb0af1414
Jakub Woyke <jaqb@sekta.pl>
2021-12-07 16:16:49 +0000

    6a

---
+++ multiply.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<map>
+#include<list>
+#include<set>
+#include<sstream>
+#include<vector>
+using namespace std;
+
+vector<string> split (string s, string delimiter) {
+  size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+  string token;
+  vector<string> res;
+
+  while ((pos_end = s.find (delimiter, pos_start)) != string::npos) {
+    token = s.substr (pos_start, pos_end - pos_start);
+    pos_start = pos_end + delim_len;
+    res.push_back (token);
+  }
+
+  res.push_back (s.substr (pos_start));
+  return res;
+}
+
+
+int main(){
+  string s;
+  cin >> s;
+  vector <int> fish(9, 0);
+  int fishc;
+  for (auto sn : split(s, ",")) {
+    ++fish[stoi(sn)];
+    ++fishc;
+  }
+  for (int i = 1; i <=80; ++i){
+    fish[(i + 6) % 9] += fish[(i - 1) % 9];
+    fishc += fish[(i - 1) % 9];
+    for (int j = i; j < i +9; ++j) {
+      cout << fish[j%9] << " ";
+    }
+    cout << "= " << fishc << endl;
+      }
+}
+
+
