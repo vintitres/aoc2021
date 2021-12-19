c07f90b890d68bf8011420cc494d3e45a695885d
Jakub Woyke <jaqb@sekta.pl>
2021-12-19 23:04:14 +0000

    19

---
+++ scan.cpp
@@ -0,0 +1,141 @@
+#include<iostream>
+#include<queue>
+#include<numeric>
+#include<algorithm>
+#include<cmath>
+#include<map>
+#include<list>
+#include<climits>
+#include<set>
+#include<sstream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+typedef tuple<int,int,int> XYZ;
+
+vector<string> split (string s, string delimiter) {
+  size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+  vector<string> res;
+  while ((pos_end = s.find (delimiter, pos_start)) != string::npos) {
+    res.push_back(s.substr (pos_start, pos_end - pos_start));
+    pos_start = pos_end + delim_len;
+  }
+  res.push_back (s.substr (pos_start));
+  return res;
+}
+
+XYZ rot(const XYZ &p,  const vector<int> &r, const vector<int> &rs) {
+  int x, y, z;
+  tie(x, y, z) = p;
+  vector<int> pp { x, y, z };
+  return make_tuple(pp[rs[0]] * r[0], pp[rs[1]] * r[1], pp[rs[2]] * r[2]);
+}
+
+XYZ shift(const XYZ &p, const XYZ &s) {
+  int x, y, z;
+  tie(x, y, z) = p;
+  int xs, ys, zs;
+  tie(xs, ys, zs) = s;
+  return make_tuple(x + xs, y + ys, z + zs);
+}
+
+void rotshift(vector<XYZ> &vp, const XYZ &s, const vector<int> &r, const vector<int> &rs) {
+  for (int i = 0; i < vp.size(); ++i) {
+    vp[i] = shift(rot(vp[i],r,rs), s);
+    int x,y,z;
+    tie(x,y,z) = vp[i];
+    //cout << x << "," << y << "," << z << endl;
+  }
+}
+
+
+tuple<bool, XYZ> match(const vector<XYZ> &s1, const vector<XYZ> &s2, const vector<int> &r, const vector<int> &rs) {
+  int x1,y1,z1,x2,y2,z2;
+  for (XYZ p1 : s1) {
+    for (XYZ p2 : s2) {
+      int points = 0;
+      tie(x1,y1,z1) = p1;
+      tie(x2,y2,z2) = rot(p2, r, rs);
+      //cout << x2 << "," << y2 << "," << z2 << endl;
+      XYZ sh = make_tuple(x1 - x2, y1 - y2, z1 -z2);
+      vector <XYZ> ss2(s2);
+      rotshift(ss2, sh, r, rs);
+      sort(ss2.begin(), ss2.end());
+      auto it = ss2.begin();
+      int i = 0;
+      for (XYZ pp1 : s1) {
+        it = lower_bound(it, ss2.end(), pp1);
+        if (*it == pp1) {
+          if (++points == 12) {  // 12
+            return make_tuple(true, sh);
+          }
+        }
+        if (s1.size() - (++i) < 12 - points) break; 
+      }
+    }
+    //cout << endl;
+  }
+  return make_tuple(false, make_tuple(0,0,0));
+}
+
+
+int main(){
+  string s;
+  vector<vector<XYZ>> scanners;
+  vector<bool> done;
+  int i = -1;
+  while (getline(cin, s)) {
+    if (s.size() == 0) continue;
+    if (s.substr(0, 4) == "--- ") {
+      scanners.resize(++i + 1);
+      done.push_back(false);
+    } else {
+      auto ss = split(s, ",");
+      scanners[i].push_back(make_tuple(stoi(ss[0]), stoi(ss[1]), stoi(ss[2])));
+    }
+  }
+  queue<int> q;
+  q.push(0);
+  done[0] = true;
+  sort(scanners[0].begin(), scanners[0].end());
+  while (!q.empty()) {
+    i = q.front();
+    q.pop();
+    for (int j = 0; j < scanners.size(); ++j) {
+      if (done[j]) continue;
+      vector<int> r(3), rs(3);
+      bool found = false;
+      XYZ sh;
+      for (r[0] = -1; r[0] <= 1 && !found; r[0] += 2) {
+        for (r[1] = -1; r[1] <= 1 && !found; r[1] += 2) {
+          for (r[2] = -1; r[2] <= 1 && !found; r[2] += 2) {
+            for (rs[0] = 0; rs[0] < 3 && !found; ++rs[0]) {
+              for (rs[1] = 0; rs[1] < 3 && !found; ++rs[1]) {
+                if (rs[0] == rs[1]) continue;
+                for (rs[2] = 0; rs[2] < 3 && !found; ++rs[2]) {
+                  if (rs[2] == rs[0] || rs[2] == rs[1])continue;
+                  tie(found, sh) = match(scanners[i], scanners[j], r, rs);
+                  if (found) {
+                    rotshift(scanners[j], sh, r, rs);
+                    sort(scanners[j].begin(), scanners[j].end());
+                    q.push(j);
+                    done[j] = true;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  set<XYZ> points;
+  for (auto s:scanners) {
+    for (auto p: s) {
+      points.insert(p);
+    }
+  }
+  cout << points.size() << endl;
+}
+
