f76621faa0ffcf4ed33288c3cca23e714c1c2013
Jakub Woyke <jaqb@sekta.pl>
2022-06-21 00:55:42 +0000

    24.1

--- vm.cpp
+++ vm.cpp
@@ -16,44 +16,49 @@ using namespace std;
 
 class State {
   public:
-  long long int w, x, y, z, inp;
-  State (long long int i) {
-    x = 0;
-    y = 0;
-    z = 0;
-    inp = i;
-  }
-  void input(char d) {
-    //cout << inp << endl;
-    //cout << inp % 10 << endl;
-    //cout << inp / 10 << endl;
-    set(d, inp % 10);
-    inp /= 10;
-  }
-  long long int get(char d) {
-    if (d == 'x') {
-      return x;
-    } else if (d == 'y') {
-      return y;
-    } else if (d == 'z') {
-      return z;
-    } else if (d == 'w') {
-      return w;
+    long long int w, x, y, z;
+    State () {
+      w = 0;
+      x = 0;
+      y = 0;
+      z = 0;
     }
-  }
-  void set(char d, long long int i) {
-    if (d == 'x') {
-      x = i;
-    } else if (d == 'y') {
-      y = i;
-    } else if (d == 'z') {
-      z = i;
-    } else if (d == 'w') {
-      w = i;
+    void input(char d, int inp) {
+      //cout << inp << endl;
+      //cout << inp % 10 << endl;
+      //cout << inp / 10 << endl;
+      set(d, inp);
     }
-  }
+    long long int get(char d) {
+      if (d == 'x') {
+        return x;
+      } else if (d == 'y') {
+        return y;
+      } else if (d == 'z') {
+        return z;
+      } else if (d == 'w') {
+        return w;
+      }
+    }
+    void set(char d, long long int i) {
+      if (d == 'x') {
+        x = i;
+      } else if (d == 'y') {
+        y = i;
+      } else if (d == 'z') {
+        z = i;
+      } else if (d == 'w') {
+        w = i;
+      }
+    }
+    bool operator<(const State& lhs) const
+    {
+          return std::tie(x,y,z,w) < std::tie(lhs.x, lhs.y, lhs.z, lhs.w);
+    }
+    void print() const {
+      cout << x << " " << y << " " << z << " " << w;
+    }
 };
-//typedef long long int[] State;
 
 vector<string> split (string s, string delimiter) {
   size_t pos_start = 0, pos_end, delim_len = delimiter.length();
@@ -68,14 +73,14 @@ vector<string> split (string s, string delimiter) {
   return res;
 }
 
-State exe(State st, string cmd) {
+State exe(State st, const string &cmd, int inp) {
   //cout << cmd << endl;
   auto ss = split(cmd, " ");
   char d = ss[1][0];
   if (ss[0] == "inp") {
-    st.input(d);
+    st.input(d, inp);
     //cout << st.get(d) << endl;
-      return st;
+    return st;
   }
   int w;
   if (ss[2][0] >= 'w' && ss[2][0] <= 'z') w = st.get(ss[2][0]);
@@ -103,6 +108,47 @@ State exe(State st, string cmd) {
   //cout << "bad"  << endl;
 }
 
+map<pair<State, int>, bool> visited;
+
+long long int run(State st, const vector<string> &cmds, int cmdi, int ii) {
+  if (cmdi == cmds.size()) {
+    if (st.get('z') == 0) {
+      //cout << " yay! ";
+      //st.print();
+      return 0;
+    }
+    else {
+      //cout << " nay ";
+      //st.print();
+      return -1;
+    }
+  }
+  string cmd = cmds[cmdi];
+  if (st.get('z') > 26*26*26*26 || visited[MP(st, cmdi)]) {
+    //cout << "!";
+    //st.print();
+    //cout << endl;
+    return -1;
+  }
+  visited[MP(st, cmdi)] = true;
+  if (cmd[0] == 'i') {
+    for (int i = 9; i > 0; --i) {
+      State nst = exe(st, cmd, i);
+      //cout << endl;
+      //for (int iii = 0; iii < ii; ++iii) {
+        //cout << " ";
+      //}
+      //cout << i;
+      long long int r = run(nst, cmds, cmdi + 1, ii + 1);
+      if (r != -1) {
+        return r * 10 + i;
+      }
+    }
+    return -1;
+  }
+  State nst = exe(st, cmd, 0);
+  return run(nst, cmds, cmdi + 1, ii);
+}
 
 int main() {
   vector<string> cmds;
@@ -110,28 +156,12 @@ int main() {
   while(getline(cin, s)) {
     cmds.push_back(s);
   }
-  for (long long int i = 99999999999999; i > 11111111111111; --i) {
-    long long int ri = 0;
-    for (long long int ii = i; ii > 0; ii /= 10) {
-      ri *= 10;
-      int r = ii % 10;
-      if (r == 0) {
-        ri = 0;
-        break;
-      }
-      ri += r;
-    }
-    if (ri == 0) continue;
-    State st(ri);
-    //cout << ri << endl;
-    for (auto s : cmds) {
-      st = exe(st, s);
-      //cout << "!" << endl;
-
-    }
-    if (st.get('z') == 0) {
-      cout << i << endl;
-    }
+  State st;
+  long long int ri = run(st, cmds, 0, 0);
+  while (ri) {
+    cout << ri % 10;
+    ri /= 10;
   }
+  cout << endl;
 }
 
